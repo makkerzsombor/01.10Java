4e73e9771531c60498e62e35f619e1429b60be25
Zsombor Makker <makker.zsombor@gmail.com>
2023-01-10 14:28:29 +0100

    beolvasás nem jo

--- src/main/java/hu/petrik/konyvtarasztali/Controller.java
+++ src/main/java/hu/petrik/konyvtarasztali/Controller.java
@@ -1,22 +1,57 @@
 package hu.petrik.konyvtarasztali;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class Controller {
 
     @FXML
     private TableView booksTableView;
     @FXML
-    private TableColumn cimCol;
+    private TableColumn<Konyv, String> cimCol;
     @FXML
-    private TableColumn szerzoCol;
+    private TableColumn<Konyv, String> szerzoCol;
     @FXML
-    private TableColumn kiadasCol;
+    private TableColumn<Konyv, Integer> kiadasCol;
     @FXML
-    private TableColumn oldalCol;
+    private TableColumn<Konyv, Integer> oldalCol;
+
+    private KonyvDB db;
 
 
+    @FXML
+    public void initialize(){
+        cimCol.setCellValueFactory(new PropertyValueFactory<>("cim"));
+        szerzoCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        kiadasCol.setCellValueFactory(new PropertyValueFactory<>("publish_year"));
+        oldalCol.setCellValueFactory(new PropertyValueFactory<>("page_count"));
+        try {
+            db = new KonyvDB();
+            db.readKonyv();
+        } catch (SQLException e) {
+            Platform.runLater(() -> {
+                alert(Alert.AlertType.WARNING, "Hiba történt az adatbázis kapcsolat kialakításakor!",
+                        e.getMessage());
+            });
+        }
+    }
+
+    private Optional<ButtonType> alert(Alert.AlertType alertType, String headerText, String contentText){
+        Alert alert = new Alert(alertType);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        return alert.showAndWait();
+    }
+
+
+
+    public void torlesClick(ActionEvent actionEvent) {
+
+    }
 }
